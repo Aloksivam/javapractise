bb690968e80c9fef65d3870dd3fecab4e9bbbccc
Alok <aloksivam@gmail.com>
2024-07-25 02:48:36 +0530

    learned about basics of string and how to detect if a particular character in string is alphanumeric or not letter or not

--- StringComplete.java
+++ StringComplete.java
@@ -1,5 +1,6 @@
 public class StringComplete {
     public static void main(String[] args) {
+        StringComplete scm = new StringComplete();
         System.out.println("we will understand about strings in java detail");
         String nm1 = "Alok";
         char[] nm = {'a','l'};
@@ -11,7 +12,18 @@ public class StringComplete {
          System.out.println(nm1.charAt(0)); //so it means it does not support indexing of strings  
          System.out.println(nm.getClass());
          System.out.println(nm1.getClass());
+         System.out.println(scm.getClass());
         //  System.out.println(nm1.getSimpleName());
+
+        //to check if a particular character is letter or not
+        // System.out.println(Character.isLetter('a'));
+        System.out.println(Character.isAlphabetic('a'));
+        // System.out.println(Character.isLetter('@'));
+
+        //Floor and normal division
+        System.out.println(5/2);
+        System.out.println("ALok".toLowerCase());
+        nm2 = nm2.toLowerCase();
 
     }
     
