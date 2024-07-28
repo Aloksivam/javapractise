3ee652e67e09dcbbdf2e730a06938fc7c098c3c4
Alok <aloksivam@gmail.com>
2024-07-28 23:25:45 +0530

    little more practise of java

---
+++ Junit/Test/TestCourse.java
@@ -0,0 +1,13 @@
+package Junit.Test;
+// import static org.junit.jupiter.api.Assertions.*;
+public class TestCourse{
+	// @Test
+	void successfullDivision() {
+		int validnum1 = 6;
+		int validnum2 = 2;
+		int result =Junit.Code.Course.div(validnum1,validnum2);
+	}
+	public static void main(String[] args) {
+		System.out.println("i am prsent for doing unit testing");
+	}
+}
