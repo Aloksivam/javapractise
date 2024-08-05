f7a50dd4265cfb9530b29f9f69165c9a36e78dc2
Alok <aloksivam@gmail.com>
2024-08-05 23:12:02 +0530

    cd

--- Arrays/SortingTest.java
+++ Arrays/SortingTest.java
@@ -1,8 +1,30 @@
 import java.util.*;
 public class SortingTest {
+    static int SecondLargest(int[] arr){
+        // arr = {}
+        int larg=arr[0];
+        int SLarg=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>larg ){
+                larg=arr[i];
+            }
+            if(arr[i]>SLarg && arr[i]!=larg){
+                SLarg = arr[i];
+            }
+        }
+        // for (int i = 1; i < arr.length; i++) {
+        //     if(arr[i]>SLarg && arr[i]!=larg){
+        //         SLarg=arr[i];
+        //     }
+        // }
+        return SLarg;
+     }
     public static void main(String[] args) {
-        int[] arr = {5,4,3,2,1};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        // int[] arr = {5,4,3,2,1};.''
+        // Arrays.sort(arr);
+        // System.out.println(Arrays.toString(arr));
+        System.out.println(SecondLargest(new int[]{4,44,31,1,0}));
+
     }
+
 }
