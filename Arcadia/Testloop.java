cdeb9d386cc3a1e7b0cb62b59c1d711acbe1842e
Alok <aloksivam@gmail.com>
2024-08-11 23:33:33 +0530

    testing fundamental working of loops

---
+++ Arcadia/Testloop.java
@@ -0,0 +1,11 @@
+public class Testloop {
+    public static void main(String[] args) {
+        int age = 12;
+        if(age<111){
+            System.out.println("alok");
+        }
+        else if(age<15){
+            System.out.println("alok");
+        }
+    }
+}
